a538bf70bdba883ddf945a03fcdf33a1deb386e8
Tenzing Sherpa <ttsherpa@umail.ucsb.edu>
2018-04-16 13:15:06 -0700

    Private and Public Route added and change firebase access of data based off of uid

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -12,8 +12,9 @@ import * as firebase from 'firebase';
   firebase.initializeApp(config);
 
   const database = firebase.database();
+  const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
 
-  export {firebase, database as default}
+  export {firebase, googleAuthProvider, database as default}
 
   // database.ref().set({
   //   name: 'Tenzing Sherpa',
