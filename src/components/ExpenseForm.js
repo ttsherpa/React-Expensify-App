a5377d3733c51a7827727ea9da6862bf3eec4add
Tenzing Sherpa <ttsherpa@umail.ucsb.edu>
2018-04-21 20:19:38 -0700

    final styles added

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -62,10 +62,10 @@ export default class ExpenseForm extends React.Component {
 
     render() {
         return (
-            <div>
-                {this.state.error && <p>{this.state.error}</p>}
-                <form onSubmit={this.onSubmit}>
+                <form className="form" onSubmit={this.onSubmit}>
+                    {this.state.error && <p className="form__error">{this.state.error}</p>}
                     <input
+                        className="text-input"
                         type = "text"
                         placeholder="Description"
                         autoFocus
@@ -73,6 +73,7 @@ export default class ExpenseForm extends React.Component {
                         onChange={this.onDescriptionChange}
                     />
                     <input  
+                        className="text-input"                        
                         type = "number"
                         placeholder="Amount"
                         value={this.state.amount}
@@ -87,14 +88,16 @@ export default class ExpenseForm extends React.Component {
                         isOutsideRange ={() => false }
                     />
                     <textarea
+                        className="textarea"
                         placeholder="Add a note for your expense (optional)"
                         value ={this.state.note}
                         onChange ={this.onNoteChange}
                     >
                     </textarea>
-                    <button>Add Expense</button>
+                    <div>
+                        <button className="button">Add Expense</button>
+                    </div>
                 </form>
-            </div>
         );
     }
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -8,9 +8,9 @@ const Header = ({startLogout}) => (
         <div className="content-container">
             <div className="header__content">
                 <Link className="header__title" to="/dashboard">
-                <h1>Expensify</h1>
+                <h1>Expensify-React</h1>
                 </Link>
-                <button onClick={startLogout}>Logout</button>
+                <button className="button button--link" onClick={startLogout}>Logout</button>
             </div>
         </div>
     </header>
